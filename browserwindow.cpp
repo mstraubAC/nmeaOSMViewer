2405ecc6c88a931ae8145b71588c3c030db669a4
Marcel Straub <straub@physik.rwth-aachen.de>
2013-01-31 16:37:34 +0100

    Adjusted use to modified gpsNMEA lib and implemented UDP listener

--- browserwindow.cpp
+++ browserwindow.cpp
@@ -1,29 +1,36 @@
 #include <QtGui>
-#include <QtWebKit>
+#include <QUdpSocket>
 #include <iostream>
+#include <iomanip>
 #include <sstream>
+#include <exception>
+#include <boost/algorithm/string.hpp>
 #include "OSMMap.h"
 #include "browserwindow.h"
 #include "Settings.h"
 #include "gpsNMEA.h"
 
 using namespace std;
+using namespace boost::algorithm;
 
-BrowserWindow::BrowserWindow(const QUrl& url)
+BrowserWindow::BrowserWindow(const QUrl& url) : fUDPSocket(0)
 {
+	// load resource file containing icons and html page
+	QResource::registerResource("resources.rcc");
+
 	// define OSM map display widget
-	fView = new OSMMap(this);
+	fView = new OSMMap(QUrl("qrc:///html/index.html" ), this);
 	connect(fView, SIGNAL(loadFinished(bool)), SLOT(finishedLoading(bool)));
 	connect(fView, SIGNAL(loadProgress(int)), SLOT(setProgress(int)));
 
 	// define quit routine
-	QPixmap quitPix("application-exit.png");
+	QPixmap quitPix(":/icons/application-exit.png");
 	QAction* quit = new QAction(quitPix, "Beenden", this);
 	quit->setShortcut(tr("CTRL+Q"));
 	connect(quit, SIGNAL(triggered()), qApp, SLOT(quit()) );
 
 	// define settings routine
-	QPixmap changeSettingsPix("preferences-other.png");
+	QPixmap changeSettingsPix(":/icons/preferences-other.png");
 	QAction* changeSettings = new QAction(changeSettingsPix, "Einstellungen", this);
 	changeSettings->setShortcut(tr("CTRL+S"));
 	connect(changeSettings, SIGNAL(triggered()), SLOT(configure()));
@@ -53,21 +60,99 @@ void BrowserWindow::configure()
 void BrowserWindow::finishedLoading(bool)
 {
 	cout << "Browser finished loading." << endl;
-
-	string fNMEA = "$GPRMC,174455,A,5124.631,N,00642.235,E,000.0,360.0,090301,000.6,W*68";
-	GPSNMEA gps;
-	bool nmeaOk = gps.isNmeaMsg(fNMEA);
-	gps.parseNmeaMsg(fNMEA);
-	cout << gps << endl;
-
-	fView->addMapMarker(gps.getLatitude(), gps.getLongitude(), gps.getCourse(), "test");
-
-
-//	fView->page()->mainFrame()->evaluateJavaScript(jQuer);
+	restartUdpListener(20000);
 }
 
 void BrowserWindow::restartUdpListener(unsigned short udpPort) {
 	cout << "BrowserWindow::restartUdpListener" << endl;
+
+	if (fUDPSocket)
+		delete fUDPSocket;
+
+	fUDPSocket = new QUdpSocket(this);
+	fUDPSocket->bind(udpPort, QUdpSocket::DontShareAddress);
+	connect(fUDPSocket, SIGNAL(readyRead()), this, SLOT(processUDP()));
+}
+
+void BrowserWindow::processUDP()
+{
+//	cout << "processUDP(): Entered" << endl;
+	while (fUDPSocket->hasPendingDatagrams()) {
+		QByteArray datagram;
+		QHostAddress host;
+		quint16 port;
+		datagram.resize(fUDPSocket->pendingDatagramSize());
+		fUDPSocket->readDatagram(datagram.data(), datagram.size(), &host, &port);
+
+		PeerType p(host.toString().toStdString(), port);
+		fUDPQueues[p] += string(datagram.data());
+
+//		cout << "processUDP(): received datagram from " << p.first << ":" << p.second << " (len=" << datagram.size() << "): " << datagram.data() << endl;
+	}
+//	cout << "processUDP(): Done" << endl;
+	parseUDPQueues();
+}
+
+void BrowserWindow::parseUDPQueues()
+{
+	try {
+		for (QueueType::iterator itHost = fUDPQueues.begin(); itHost != fUDPQueues.end(); ++itHost) {
+			unsigned int i = 0;
+			cout << "Processing queue of " << itHost->first.first << ":" << itHost->first.second << endl;
+			unsigned int start, len = 0;
+			bool isOk = fGps.isNmeaMsg(itHost->second, &start, &len);
+			while (isOk || len > 0) {
+				string dgm = itHost->second.substr(start, len);
+				if (!isOk && len > 0) {
+					// bad dgm
+					cerr << i << "(" << start << ", " << len << ") is BAD: " << dgm << endl;
+				}
+				else {
+					// dgm ist ok
+					trim(dgm);
+					cout << i << "(" << start << ", " << len << "): " << dgm << endl;
+					if (fGps.parseNmeaMsg(dgm)) {
+						// it is s datagram which updated our position info
+						stringstream ss;
+						ss  << "Altitude:     " << setprecision(5) << fGps.getAltitude() << "m<br>"
+							<< "Ground speed: " << setprecision(4) << fGps.getGroundSpeed() << "m/s<br>"
+							<< "Sat count:    " << fGps.getSatCount() << "<br>"
+							<< "Precission:   " << setprecision(1) << fGps.getHorizontalPrecision() << "m<br>";
+						stringstream st;
+						st  << "H=" << setprecision(5) << fGps.getAltitude() << "m / "
+							<< "V=" << setprecision(4) << fGps.getGroundSpeed() << "m/s / "
+							<< "S=" << fGps.getSatCount();
+						if (fView->existsMapMarker(fId)) {
+							cout << "Update" << endl;
+							fView->updateMapMarker(fId, fGps.getLatitude(), fGps.getLongitude(), fGps.getCourse(), st.str(), ss.str());
+						}
+						else {
+							fId = fView->addMapMarker(fGps.getLatitude(), fGps.getLongitude(), fGps.getCourse(), st.str(), ss.str());
+						}
+					}
+				}
+
+				// finally remove dgm from string
+				try {
+					itHost->second = itHost->second.substr(start+len, string::npos);
+				}
+				catch (const exception& e){
+					cerr << "ERROR: itHost->second = itHost->second.substr(start+len, string::npos);" << endl
+					     << "   -> string=" << itHost->second << endl
+					     << "   -> start =" << start << endl
+					     << "   -> len   =" << len << endl
+					     << "   -> length=" << itHost->second.length() << endl;
+				}
+				i++;
+				start = 0;
+				len = 0;
+				isOk = fGps.isNmeaMsg(itHost->second, &start, &len);
+			}
+		}
+	}
+	catch (const exception& e) {
+		cerr << "Got an exception in BrowserWindo::parseUDPQueues(): " << e.what() << endl;
+	}
 }
 
 void BrowserWindow::setProgress(int p) {
--- browserwindow.h
+++ browserwindow.h
@@ -1,7 +1,11 @@
 #include <QtGui>
+#include <map>
+#include <string>
+#include "gpsNMEA.h"
 
 class QWebView;
 class QLineEdit;
+class QUdpSocket;
 class OSMMap;
 
 class BrowserWindow: public QMainWindow
@@ -13,11 +17,20 @@ class BrowserWindow: public QMainWindow
 		void restartUdpListener(unsigned short udpPort);
 
 	private:
+		typedef std::pair<std::string, unsigned short> PeerType;
+		typedef std::map<PeerType, std::string> QueueType;
+		GPSNMEA fGps;
 		OSMMap* fView;
+		QUdpSocket* fUDPSocket;
+		QueueType fUDPQueues;
 		int fPort;
+		int fId;
+
+		void parseUDPQueues();
 
 	protected slots:
 		void finishedLoading(bool);
 		void setProgress(int p);
 		void configure();
+		void processUDP();
 };
