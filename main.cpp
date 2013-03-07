a817ad59603f56e645dc6354bc2fc31a0e027286
Marcel Straub <straub@physik.rwth-aachen.de>
2013-03-07 13:49:53 +0100

    Set globally company information and project name

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <QWidget>
 #include <QtGui>
 #include <QThread>
+#include <QSettings>
 #include <iostream>
 #include <string>
 
@@ -13,6 +14,10 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
+
+	QCoreApplication::setOrganizationName("TechnoTeam Informationssysteme GmbH");
+	QCoreApplication::setOrganizationDomain("techno-team.de");
+	QCoreApplication::setApplicationName("OSMMap");
 
 	BrowserWindow browser;
 	browser.show();
